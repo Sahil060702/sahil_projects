494e6d8faf7d23e02b2daf052b53bbad745e85dc
Sahil060702 <142890912+Sahil060702@users.noreply.github.com>
2023-11-18 22:05:39 +0530

    Update prime.py

--- python/prime.py
+++ python/prime.py
@@ -1,5 +1,18 @@
-x=int(input("Enter a number"))
+'''x=int(input("Enter a number"))
 if(x%2==0 and x%x==0 and x>1):
     print("Not a prime number")
 else:
-    print("Prime number")
+    print("Prime number")'''
+    
+    
+num = int(input("Enter a number: "))
+
+if (num == 1):
+    print(num, "is not a prime number")
+elif (num > 1):
+   for i in range(2,num):
+       if (num % i == 0):
+           print(num,"is not a prime number")
+           break
+   else:
+       print(num,"is a prime number")
