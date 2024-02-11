77f48c39516d31ab5d13523626e98d32b1564faf
Sahil060702 <142890912+Sahil060702@users.noreply.github.com>
2024-02-11 18:22:48 +0530

    n

--- OnePlus_django/OnePlus/core/urls.py
+++ OnePlus_django/OnePlus/core/urls.py
@@ -6,4 +6,23 @@ urlpatterns=[
     path("navbar/",views.navbar),
     path("footer/",views.footer),
     path("products/",views.products),
+    path("contact/",views.contact),
+    path("cart/",views.cart),
+    path("account/",views.account),
+    #Urls for products on Homepage
+    path("buds2/",views.buds2),
+    path("nord_buds/",views.nord_buds),
+    path("bullets_z2/",views.bullets_z2),
+    #Urls for products on Products page
+    path("gaming_bundle/",views.gaming_bundle),
+    path("charging_bundle/",views.charging_bundle),
+    path("music_bundle/",views.music_bundle),
+    path("op_12r/",views.op_12r),
+    path("op_11r/",views.op_11r),
+    path("op_11_5g/",views.op_11_5g),
+    path("pad_go/",views.pad_go),
+    path("op_pad/",views.op_pad),
+    path("folio_case/",views.folio_case),
+
+
 ]
--- OnePlus_django/OnePlus/core/views.py
+++ OnePlus_django/OnePlus/core/views.py
@@ -12,3 +12,50 @@ def footer(request):
 
 def products(request):
     return render(request,"core/products.html")
+
+def contact(request):
+    return render(request,"core/contact.html")
+
+def cart(request):
+    return render(request,"core/cart.html")
+
+def account(request):
+    return render(request,"core/account.html")
+
+#Views for products on Homepage
+def buds2(request):
+    return render(request,"core/Buds_pro_2.html")
+
+def nord_buds(request):
+    return render(request,"core/nord_buds_2.html")
+
+def bullets_z2(request):
+    return render(request,"core/bullets_z2.html")
+
+#Urls for products on Products page
+def gaming_bundle(request):
+    return render(request,"core/gaming_bundle.html")
+
+def charging_bundle(request):
+    return render(request,"core/charging_bundle.html")
+
+def music_bundle(request):
+    return render(request,"core/music_bundle.html")
+
+def op_12r(request):
+    return render(request,"core/op_12r.html")
+
+def op_11r(request):
+    return render(request,"core/op_11r.html")
+
+def op_11_5g(request):
+    return render(request,"core/op_11.html")
+
+def pad_go(request):
+    return render(request,"core/pad_go.html")
+
+def op_pad(request):
+    return render(request,"core/op_pad.html")
+
+def folio_case(request):
+    return render(request,"core/folio_case.html")
